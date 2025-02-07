c3511f7fd745c1f6f19abead295f268dcc5a9869
lockdown4 <deepmungra501@gmail.com>
2025-02-07 16:03:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function colorchange(event) {
 }
 
 function colorchanges(event) {
-    let colors = ['	#B22222', 'black', 'gold', '	#B22333'];
+    let colors = ['black', 'gold'];
     let randomColor = colors[Math.floor(Math.random() * colors.length)];  
     document.body.style.backgroundColor = randomColor;
 }
